d3f74996cfd9d72578799897d14383be6517410e
Egor S. Orlov <e.s.orlov@gmail.com>
2021-11-24 00:17:40 +0300

    Make this work

---
+++ 01-1.hello_module/hello_module.c
@@ -0,0 +1,12 @@
+#include <linux/module.h>
+
+int init_module(void) {
+	printk(KERN_INFO "Hello world loadding");
+	return 0;
+}
+
+void cleanup_module(void) {
+	printk(KERN_INFO "Hello world cleanup");
+}
+
+MODULE_LICENSE("GPL");
