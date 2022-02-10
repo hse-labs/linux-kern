9491015978529c254d6254659785f65ca41b3093
Egor S. Orlov <e.s.orlov@gmail.com>
2022-02-10 21:30:30 +0300

    Minor fixes

--- 07-5.keylog/keylog.c
+++ 07-5.keylog/keylog.c
@@ -18,18 +18,16 @@
 
 #define BUFFER_SIZE 1024
 
-struct work_struct *hardwork;
-
 struct keylog_data {
-	char buf[BUFFER_SIZE];
-	size_t put_idx, get_idx, count;
-	spinlock_t lock; // переменная блокировка
-	struct work_struct workq;
+	char buf[BUFFER_SIZE];		// кольцевой буфер
+	size_t put_idx, get_idx, count; // индексы
+	spinlock_t lock;		// переменная блокировка
+	struct work_struct workq;	// структура задания
 } k_data;
 
 static int irq = I8042_K_IRQ, irq_counter = 0;
 
-static void put_char(struct keylog_data *kdata, char c) {
+static void cycbuf_put_char(struct keylog_data *kdata, char c) {
 	if (kdata->count >= BUFFER_SIZE)
 		return;
 
@@ -38,7 +36,7 @@ static void put_char(struct keylog_data *kdata, char c) {
 	kdata->count++;
 }
 
-static bool get_char(char *c, struct keylog_data *kdata) {
+static bool cycbuf_get_char(char *c, struct keylog_data *kdata) {
 	if (kdata->count > 0) {
 		*c = kdata->buf[kdata->get_idx];
 		kdata->get_idx = (kdata->get_idx + 1) % BUFFER_SIZE;
@@ -48,8 +46,22 @@ static bool get_char(char *c, struct keylog_data *kdata) {
 	return false;
 }
 
-static void reset_buffer(struct keylog_data *kdata)
-{
+static int cycbuf_get_size(struct keylog_data *kdata) {
+	return kdata->count;
+}
+
+static int cycbuf_get_str(char *str, struct keylog_data *kdata) {
+	char ch;
+	int i = 0;
+	while (cycbuf_get_char(&ch, kdata)) {
+		*(str+i)=ch;
+		i++;
+	}
+	str[i]=0;
+	return i;
+}
+
+static void cycbuf_reset_buffer(struct keylog_data *kdata) {
 	kdata->count = 0;
 	kdata->put_idx = 0;
 	kdata->get_idx = 0;
@@ -80,30 +92,23 @@ static int scancode2ascii(u8 scancode) {
 		return *(row3 + scancode - 0x1e);
 	if (scancode >= 0x2c && scancode <= 0x32)
 		return *(row4 + scancode - 0x2c);
-	if (scancode == 0x39)
-		return ' ';
-	if (scancode == 0x1c)
-		return '\n';
+	if (scancode == 0x39)	return ' ';
+	if (scancode == 0x1c)	return '\n';
+	if (scancode == 12)	return '-';
+	if (scancode == 53)	return '/';
 	return '?';
 }
 
 static void flush_data( struct work_struct *work ) {
-	char ch;
-	char *buffer;
-	int i;
 	unsigned long flags;
 	struct keylog_data *kdata = container_of(work, struct keylog_data, workq);
 	printk (KERN_INFO "07-5 - In Workqueue. Flushing\n");
-	buffer = kmalloc(kdata->count,GFP_KERNEL);
-	i=0;
+	char *buffer = kmalloc(cycbuf_get_size(kdata)+1,GFP_KERNEL);
 	// Включаем блокировку
 	spin_lock_irqsave(&kdata->lock,flags);
-	while (get_char(&ch, kdata)) {// критическая секция
-		*(buffer+i)=ch;
-		i++;
-	}
+	int len = cycbuf_get_str((char*)buffer,kdata);
 	spin_unlock_irqrestore(&kdata->lock,flags); // выходим из секции
-	printk (KERN_INFO "07-5 - Buffer: [%ld] %s\n",i,buffer);
+	printk (KERN_INFO "07-5 - Buffer: [%d] %s\n",len,buffer);
 	kfree(buffer);
 	return;
 }
@@ -115,16 +120,12 @@ static irqreturn_t my_interrupt( int irq, void *dev_id ) {
 	struct keylog_data *kdata = (struct keylog_data *)dev_id;
 	scancode = i8042_read_data();
 	ch = scancode2ascii(scancode);
-	if (pressed=is_key_pressed(scancode)) {
-		printk( KERN_INFO "07-5 - In the ISR: counter = %d, scancode: %d, char: %c pressed\n", \
-				irq_counter,scancode,ch );
+	if ((pressed=is_key_pressed(scancode))) {
 		// Включаем блокировку
 		spin_lock(&kdata->lock);
-		put_char(kdata, ch); // критическая секция
+		cycbuf_put_char(kdata, ch); // критическая секция
 		spin_unlock(&kdata->lock); // выходим из секции
 	} else {
-		printk( KERN_INFO "07-5 - In the ISR: counter = %d, scancode: %d, char: %c released\n", \
-				irq_counter,scancode - RELEASED_MASK, ch );
 		if ( ch == '\n' ) {
 			printk (KERN_INFO "07-5 - Got new line. Sheduling flush workqueue\n");
 			schedule_work(&kdata->workq);
@@ -135,30 +136,31 @@ static irqreturn_t my_interrupt( int irq, void *dev_id ) {
 
 static int __init my_init( void ) {
 	unsigned long flags;
-        // Инициализация спин-блокировки
-        spin_lock_init(&k_data.lock);
-        // Старт блокировки
-        spin_lock_irqsave(&k_data.lock,flags);
-        reset_buffer(&k_data);  // критичная секция
-        spin_unlock_irqrestore(&k_data.lock,flags);
-	// Разблокировка
+        spin_lock_init(&k_data.lock);			// Инициализация спин-блокировки
+        spin_lock_irqsave(&k_data.lock,flags);		// Старт блокировки
+        cycbuf_reset_buffer(&k_data);  			// Критическая секция
+        spin_unlock_irqrestore(&k_data.lock,flags);	// Разблокировка
 	if ( request_irq( irq, my_interrupt, IRQF_SHARED, "my_interrupt", &k_data ) )
 		return -1;
 	printk( KERN_INFO "07.5 - Successfully loading ISR handler on IRQ %d\n", irq );
 	INIT_WORK( &k_data.workq, flush_data );	
 	return 0;
 }
-
 static void __exit my_exit( void ) {
 	unsigned long flags;
+	char *buffer;
+	int i = 0;
+	char ch;
 	synchronize_irq( irq );
 	free_irq( irq, &k_data );
 	printk( KERN_INFO "07.5 - Successfully unloading, irq_counter = %d\n", irq_counter );
 	flush_scheduled_work();
-	spin_lock_irqsave(&k_data.lock,flags); // Начало критической секции
-	k_data.buf[k_data.count]=0;
-	printk (KERN_INFO "07-5 - Buffer: [%ld] %s\n",k_data.count,k_data.buf);
+	buffer = kmalloc(cycbuf_get_size(&k_data)+1,GFP_KERNEL);
+	spin_lock_irqsave(&k_data.lock,flags); 		// Начало критической секции
+	int len = cycbuf_get_str((char*)buffer,&k_data);
+	printk (KERN_INFO "07-5 - Buffer: [%d] %s\n",len,buffer);
 	spin_unlock_irqrestore(&k_data.lock,flags);
+	kfree(buffer);
 }
 module_init( my_init );
 module_exit( my_exit );
